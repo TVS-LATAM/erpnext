fdb6521e6a4fe0492c92dc61c59575e1624e28db
Andres rodriguez santos <52182239+andresrodriguezsantos44@users.noreply.github.com>
2025-04-21 15:38:02 -0500

    feat: validate payment id not null

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.js
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.js
@@ -618,6 +618,17 @@ cur_frm.set_query("asset", "items", function (doc, cdt, cdn) {
 });
 
 frappe.ui.form.on('Sales Invoice', {
+	before_save: function (frm) {
+		if (!frm.doc.payment_id || frm.doc.payment_id == "") {
+			const generateCompactID = () => {
+				const timestamp = Date.now().toString().slice(-8);
+				const randomPart = frappe.utils.randomString(6);
+				return `Auto-${timestamp}-${randomPart}`;
+			};
+			
+			frm.doc.payment_id = generateCompactID();
+		}
+	},
 	setup: function (frm) {
 		frm.set_value('selling_price_list', "Retail");
 		frm.add_fetch('customer', 'tax_id', 'tax_id');
@@ -1111,4 +1122,3 @@ function refreshSalesInvoicesFields(frm) {
 	frm.trigger("calculate_taxes_and_totals");
 	frm.refresh_fields(['rate', 'total', 'grand_total', 'net_total']);
 }
-
