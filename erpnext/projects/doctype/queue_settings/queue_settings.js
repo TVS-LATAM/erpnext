925d913a694c99c586dbf03648630baa87569cef
Andres rodriguez santos <andresrodriguezsantos44@gmail.com>
2026-04-30 15:07:21 -0500

    feat: lanes added

--- erpnext/projects/doctype/queue_settings/queue_settings.js
+++ erpnext/projects/doctype/queue_settings/queue_settings.js
@@ -2,11 +2,29 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Queue Settings", {
+  refresh: function (frm) {
+    frm.trigger('toggle_lanes');
+  },
+  lanes_enabled: function (frm) {
+    frm.trigger('toggle_lanes');
+  },
+  toggle_lanes: function (frm) {
+    frm.toggle_display('sb_lanes', frm.doc.lanes_enabled);
+    frm.toggle_display('fast_cars_per_day', frm.doc.lanes_enabled);
+    frm.toggle_display('heavy_cars_per_day', frm.doc.lanes_enabled);
+  },
   validate: function (frm) {
-    const cars_per_day = Number(frm.doc.cars_per_day)
-    if (cars_per_day <= 0 || !Number.isInteger(cars_per_day)) {
-      frappe.msgprint(__('The value for {0} must be greater than 0.', [__('Cars per Day')]));
-      frappe.validated = false;
+    const check_positive = (val, label) => {
+      const num = Number(val);
+      if (num <= 0 || !Number.isInteger(num)) {
+        frappe.msgprint(__('The value for {0} must be greater than 0.', [__(label)]));
+        frappe.validated = false;
+      }
+    };
+
+    if (frm.doc.lanes_enabled) {
+      check_positive(frm.doc.fast_cars_per_day, 'Fast Cars per Day');
+      check_positive(frm.doc.heavy_cars_per_day, 'Heavy Cars per Day');
     }
   }
 });
--- erpnext/projects/doctype/queue_settings/queue_settings.py
+++ erpnext/projects/doctype/queue_settings/queue_settings.py
@@ -9,13 +9,31 @@ import json
 
 
 class QueueSettings(Document):
-	def on_update(self): 
-		print(self.cars_per_day)
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from erpnext.projects.doctype.job_workload_item.job_workload_item import JobWorkloadItem
+		from frappe.types import DF
+
+		auto_move_paused: DF.Check
+		aws_url: DF.Data | None
+		job_workload_status: DF.Table[JobWorkloadItem]
+		fast_cars_per_day: DF.Int
+		heavy_cars_per_day: DF.Int
+		lanes_enabled: DF.Check
+		vacations_mode: DF.Check
+	# end: auto-generated types
+	def on_update(self):
 		if self.aws_url:
 			url = f"{self.aws_url}/queue-settings/updated"
+			data = self.as_dict(convert_dates_to_str=True)
+
 			make_post_request(
 				url,
 				headers={"Content-Type": "application/json"},
-				data=json.dumps(self.as_dict(convert_dates_to_str=True)),
+				data=json.dumps(data),
 			)
 	pass
