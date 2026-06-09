9199d561b064bc57550cc80b0ad5b8c5d1d442a3
Andres rodriguez santos <andresrodriguezsantos44@gmail.com>
2026-06-09 11:25:40 -0500

    feat: double lane active/inactive

--- erpnext/projects/doctype/queue_settings/queue_settings.js
+++ erpnext/projects/doctype/queue_settings/queue_settings.js
@@ -5,15 +5,22 @@ frappe.ui.form.on("Queue Settings", {
   refresh: function (frm) {
     frm.trigger('toggle_lanes');
   },
-  lanes_enabled: function (frm) {
+  fast_lane_enabled: function (frm) {
+    frm.trigger('toggle_lanes');
+  },
+  heavy_lane_enabled: function (frm) {
     frm.trigger('toggle_lanes');
   },
   toggle_lanes: function (frm) {
-    frm.toggle_display('sb_lanes', frm.doc.lanes_enabled);
-    frm.toggle_display('fast_cars_per_day', frm.doc.lanes_enabled);
-    frm.toggle_display('heavy_cars_per_day', frm.doc.lanes_enabled);
+    frm.toggle_display('fast_cars_per_day', frm.doc.fast_lane_enabled);
+    frm.toggle_display('heavy_cars_per_day', frm.doc.heavy_lane_enabled);
   },
   validate: function (frm) {
+    if (!frm.doc.fast_lane_enabled && !frm.doc.heavy_lane_enabled) {
+      frappe.msgprint(__('At least one lane must be enabled.'));
+      frappe.validated = false;
+    }
+
     const check_positive = (val, label) => {
       const num = Number(val);
       if (num <= 0 || !Number.isInteger(num)) {
@@ -22,8 +29,10 @@ frappe.ui.form.on("Queue Settings", {
       }
     };
 
-    if (frm.doc.lanes_enabled) {
+    if (frm.doc.fast_lane_enabled) {
       check_positive(frm.doc.fast_cars_per_day, 'Fast Cars per Day');
+    }
+    if (frm.doc.heavy_lane_enabled) {
       check_positive(frm.doc.heavy_cars_per_day, 'Heavy Cars per Day');
     }
   }
