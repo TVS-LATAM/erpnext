72465728ae15298c6eb623f947cb999add095f6b
Julian Alzate <julian.alzate96@gmail.com>
2026-06-23 20:21:44 +0000

    feat(quotation-builder): add transmission-code search flag to Labour Job
    Add a `use_transmission_search` check field to Labour Job and seed it on the
    Gearbox job. When set, the quotation builder resolves the job's parts by
    matching the Project transmission_code against each Item's transmission-code
    compatibility table.
    
    - labour_job.json: new use_transmission_search field
    - manuren_import.py: extend JOB_SEED tuples with the flag (Gearbox = 1) and
      backfill it on existing jobs in ensure_masters()

--- erpnext/selling/doctype/manuren_import/manuren_import.py
+++ erpnext/selling/doctype/manuren_import/manuren_import.py
@@ -21,14 +21,14 @@ VARIANT_SEED = [
 ]
 
 JOB_SEED = [
-	# job_name, dutch_aliases, diagnosis_keywords, project_part_fields (anchor OEM part), item_match_tokens, use_oe_crossref
-	("Oil Change", ["Oliewissel"], ["oil change", "olie", "oliewissel"], [], ["oil", "olie", "oliewissel"], 0),
-	("Mechatronics", ["Mechatronic vervangen", "Mechatronic vervangen / TCU", "Mechatronic"], ["mechatronic", "tcu"], ["mechatronic"], ["mec", "mechatronic", "tcu"], 0),
-	("Clutch", ["Koppeling vervangen"], ["clutch", "koppeling"], ["clutch"], ["clu", "kop", "koppeling", "clutch"], 1),
-	("Flywheel", ["Vliegwiel vervangen"], ["flywheel", "vliegwiel"], ["flywheel"], ["fly", "vli", "vliegwiel", "flywheel"], 1),
-	("Gearbox", ["Versnellingsbak vervangen"], ["gearbox", "transmission", "versnellingsbak"], ["dsg_gearbox"], ["bak", "gear", "versnellingsbak", "gearbox"], 0),
-	("Clutch + Flywheel", ["Koppeling + Vliegwiel vervangen"], ["clutch and flywheel", "koppeling + vliegwiel"], ["clutch", "flywheel"], ["clu", "kop", "koppeling", "fly", "vli", "vliegwiel"], 1),
-	("Ride-height Sensor", ["Rijstand sensor vervangen"], ["ride height sensor", "rijstand", "level sensor"], [], ["rijstand", "niveau", "level", "sensor"], 0),
+	# job_name, dutch_aliases, diagnosis_keywords, project_part_fields, item_match_tokens, use_oe_crossref, use_oe_pn_search, use_transmission_search
+	("Oil Change", ["Oliewissel"], ["oil change", "olie", "oliewissel"], [], ["oil", "olie", "oliewissel"], 0, 0, 0),
+	("Mechatronics", ["Mechatronic vervangen", "Mechatronic vervangen / TCU", "Mechatronic"], ["mechatronic", "tcu"], ["mechatronic"], ["mec", "mechatronic", "tcu"], 0, 1, 0),
+	("Clutch", ["Koppeling vervangen"], ["clutch", "koppeling"], ["clutch"], ["clu", "kop", "koppeling", "clutch"], 1, 0, 0),
+	("Flywheel", ["Vliegwiel vervangen"], ["flywheel", "vliegwiel"], ["flywheel"], ["fly", "vli", "vliegwiel", "flywheel"], 1, 0, 0),
+	("Gearbox", ["Versnellingsbak vervangen"], ["gearbox", "transmission", "versnellingsbak"], ["dsg_gearbox"], ["bak", "gear", "versnellingsbak", "gearbox"], 0, 0, 1),
+	("Clutch + Flywheel", ["Koppeling + Vliegwiel vervangen"], ["clutch and flywheel", "koppeling + vliegwiel"], ["clutch", "flywheel"], ["clu", "kop", "koppeling", "fly", "vli", "vliegwiel"], 1, 0, 0),
+	("Ride-height Sensor", ["Rijstand sensor vervangen"], ["ride height sensor", "rijstand", "level sensor"], [], ["rijstand", "niveau", "level", "sensor"], 0, 0, 0),
 ]
 
 DSG_CODE_RE = re.compile(r"^(dq|dl)\d{3}$")
@@ -72,7 +72,7 @@ def ensure_masters():
 				}
 			).insert(ignore_permissions=True)
 
-	for name, aliases, keywords, part_fields, item_tokens, use_crossref in JOB_SEED:
+	for name, aliases, keywords, part_fields, item_tokens, use_crossref, use_pn_search, use_trans in JOB_SEED:
 		if not frappe.db.exists("Labour Job", name):
 			frappe.get_doc(
 				{
@@ -83,6 +83,8 @@ def ensure_masters():
 					"project_part_fields": ", ".join(part_fields),
 					"item_match_keywords": ", ".join(item_tokens),
 					"use_oe_crossref": use_crossref,
+					"use_oe_pn_search": use_pn_search,
+					"use_transmission_search": use_trans,
 				}
 			).insert(ignore_permissions=True)
 		else:
@@ -93,6 +95,10 @@ def ensure_masters():
 				frappe.db.set_value("Labour Job", name, "item_match_keywords", ", ".join(item_tokens))
 			if use_crossref and not frappe.db.get_value("Labour Job", name, "use_oe_crossref"):
 				frappe.db.set_value("Labour Job", name, "use_oe_crossref", use_crossref)
+			if use_pn_search and not frappe.db.get_value("Labour Job", name, "use_oe_pn_search"):
+				frappe.db.set_value("Labour Job", name, "use_oe_pn_search", use_pn_search)
+			if use_trans and not frappe.db.get_value("Labour Job", name, "use_transmission_search"):
+				frappe.db.set_value("Labour Job", name, "use_transmission_search", use_trans)
 
 
 def _build_lookups():
